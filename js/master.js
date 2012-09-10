4a87493cfdd424d46672fb4290907ef72729be77
Kamule <benjamin.herbomez@gmail.com>
2012-09-10 22:40:39 +0200

    Désactivation du scroll touch

--- js/master.js
+++ js/master.js
@@ -7,6 +7,17 @@ jQuery(window).ready(function(){
 		log();
 	});
 	
+	var ar=new Array(33,34,35,36,37,38,39,40);
+
+	jQuery(document).keydown(function(e) {
+		 var key = e.which;
+		  if($.inArray(key,ar) > -1) {
+			  e.preventDefault();
+			  return false;
+		  }
+		  return true;
+	});
+		
 });
 
 function selectScreen(screen){
